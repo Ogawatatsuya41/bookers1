5a785f1d49c70a9bc6cdd8a24df430d10104f7f6
Tatsuya <82919942+Ogawatatsuya41@users.noreply.github.com>
2023-06-17 06:25:19 +0000

    forth commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,8 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book.save
+    redirect_to book_path(@book.id)
   end
   def index
     @books = Book.all  
